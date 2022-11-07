640c2dea7dba7b90a063c7a0df1ca0f36c3e3c0b
kennywindis <75937718+kennywindis@users.noreply.github.com>
2022-11-08 00:44:23 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -5,12 +5,14 @@ void sort_suit(deck_node_t **deck);
 void sort_val(deck_node_t **deck);
 void swap(deck_node_t **deck, deck_node_t *card1, deck_node_t *card2);
 int vcmp(const card_t *card1, const card_t *card2);
+
 /**
  * sort_deck - sorts a 52 card deck
  * @deck: deck to sort
  *
  * Return: Always 0 (ok)
  */
+
 void sort_deck(deck_node_t **deck)
 {
 	if (deck == NULL || *deck == NULL)
@@ -23,6 +25,7 @@ void sort_deck(deck_node_t **deck)
  * sort_suit - sorts a 52 card deck by suit
  * @deck: deck to sort
  */
+
 void sort_suit(deck_node_t **deck)
 {
 	deck_node_t *forw, *tmp;
@@ -43,6 +46,7 @@ void sort_suit(deck_node_t **deck)
  * sort_val - sorts a 52 card deck staticly by value
  * @deck: deck to sort
  */
+
 void sort_val(deck_node_t **deck)
 {
 	deck_node_t *f, *tmp;
@@ -66,6 +70,7 @@ void sort_val(deck_node_t **deck)
  * @card1: The first card to swap
  * @card2: The second card to swap
  */
+
 void swap(deck_node_t **deck, deck_node_t *card1, deck_node_t *card2)
 {
 	deck_node_t *prev, *next;
@@ -92,6 +97,7 @@ void swap(deck_node_t **deck, deck_node_t *card1, deck_node_t *card2)
  *
  * Return: 1 if less than, 0 if greater or equal to
  */
+
 int vcmp(const card_t *card1, const card_t *card2)
 {
 	char char1 = card1->value[0], char2 = card2->value[0];
