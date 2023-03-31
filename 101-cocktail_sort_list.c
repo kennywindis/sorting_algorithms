b6134d174091beb675a6fd607cdc87ccbd8ee81d
kennywindis <kennyphilip100@gmail.com>
2023-03-31 05:13:50 -0700

    sa

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,38 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
-
 /**
- * cocktail_sort_list - function that sorts a linked list using coctail
- * @list: pointer to first node in linked list
+ * quick_sort - function that sorts an array of integers
+ *              in ascending order using the Quick sort algorithm
+ * @array: array
+ * @size: array's size
+ * Return: void
  */
-void cocktail_sort_list(listint_t **list)
+void quick_sort(int *array, size_t size)
 {
-	listint_t *cursor;
-	int cont = 1;
-
-	if (list == NULL || *list == NULL)
+	if (array == NULL || size < 2)
 		return;
 
-	while (cont != 0)
+	quick_s(array, 0, size - 1, size);
+}
+
+/**
+ * partition - partition
+ * @array: array
+ * @lo: lower
+ * @hi: higher
+ * @size: array's size
+ * Return: i
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int i = lo - 1, j = lo;
+	int pivot = array[hi], aux = 0;
+
+	for (; j < hi; j++)
 	{
-		cont = 0;
-		for (cursor = *list; cursor->next != NULL; cursor = cursor->next)
+		if (array[j] < pivot)
 		{
-			if (cursor->n > cursor->next->n)
+			i++;
+			if (array[i] != array[j])
 			{
-				swap_forward(&cursor, list);
-				cont++;
+				aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+				print_array(array, size);
 			}
 		}
-		for (; cursor->prev != NULL; cursor = cursor->prev)
-		{
-			if (cursor->n < cursor->prev->n)
-			{
-				swap_backward(&cursor, list);
-				cont++;
-			}
-		}
+	}
+	if (array[i + 1] != array[hi])
+	{
+		aux = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = aux;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_s - quick sort
+ * @array: given array
+ * @lo: lower
+ * @hi:higher
+ * @size: array's size
+ * Return: void
+ */
+void quick_s(int *array, int lo, int hi, size_t size)
+{
+	int pivot;
+
+	if (lo < hi)
+	{
+		pivot = partition(array, lo, hi, size);
+		quick_s(array, lo, pivot - 1, size);
+		quick_s(array, pivot + 1, hi, size);
 	}
 }
